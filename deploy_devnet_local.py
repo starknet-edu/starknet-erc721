1082fe7f4c763cb732119f8df851caa34e3f15d5
cryptoleek-eth <cryptoleek.eth@gmail.com>
2023-01-26 15:11:51 +1100

    extract max_fee variables

--- deploy_devnet_local.py
+++ deploy_devnet_local.py
@@ -2,15 +2,16 @@ import subprocess, json
 from utils import str_to_felt, felt_to_str
 
 # essential settings
-network = "devnet"
-account_addr = "0x33507ff2edf12c12c73d0b6d1d90de9fac12a355de1097ab305249612451919"
-salt = 1234
+network = "testnet2"
+account_addr = "0x0059afd418b4f3c3f18d05ca3ac333d382b3269537204acd314d6e269dad9e19"
+salt = 5658
 tderc20_addr = "0x"
 player_registry_addr = "0x"
 dummytoken_addr = "0x"
 tderc721_metadata_addr = "0x"
 evaluator_addr = "0x"
-pkey = ".pkey_local"
+pkey = ".pkey_testnet2"
+max_fee = "9999999"
 
 def run_command(cmd):
   out = subprocess.check_output(cmd.split(" "))
@@ -37,13 +38,13 @@ def test():
 def deploy_TDERC20(token_name_str, token_symbol_str):
   contract = "TDERC20"
   print("DECLARE " + contract)
-  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto --json")
+  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} --wait-for-acceptance --json")
 
   print("DEPLOY " + contract)
   token_name = str_to_felt(token_name_str)
   token_symbol = str_to_felt(token_symbol_str)
   class_hash = json.loads(out)['class_hash']
-  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto -i {token_name} {token_symbol} 0 0 {account_addr} {account_addr} --json")
+  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} -i {token_name} {token_symbol} 0 0 {account_addr} {account_addr} --wait-for-acceptance --json")
 
   global tderc20_addr
   tderc20_addr = json.loads(out)['contract_address']
@@ -54,11 +55,11 @@ def deploy_TDERC20(token_name_str, token_symbol_str):
 def deploy_players_registry():
   contract = "players_registry"
   print("DECLARE " + contract)
-  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto --json")
+  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} --wait-for-acceptance --json")
 
   print("DEPLOY " + contract)
   class_hash = json.loads(out)['class_hash']
-  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto -i {account_addr} --json")
+  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} -i {account_addr} --wait-for-acceptance --json")
 
   global player_registry_addr
   player_registry_addr = json.loads(out)['contract_address']
@@ -69,13 +70,13 @@ def deploy_players_registry():
 def deploy_dummy_token(token_name_str, token_symbol_str):
   contract = "dummy_token"
   print("DECLARE " + contract)
-  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto --json")
+  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} --wait-for-acceptance --json")
 
   print("DEPLOY " + contract)
   token_name = str_to_felt(token_name_str)
   token_symbol = str_to_felt(token_symbol_str)
   class_hash = json.loads(out)['class_hash']
-  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto -i {token_name} {token_symbol} 100000000000000000000 0 {account_addr} --json")
+  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} -i {token_name} {token_symbol} 100000000000000000000 0 {account_addr} --wait-for-acceptance --json")
 
   global dummytoken_addr
   dummytoken_addr = json.loads(out)['contract_address']
@@ -86,7 +87,7 @@ def deploy_dummy_token(token_name_str, token_symbol_str):
 def deploy_erc721(token_name_str, token_symbol_str, token_metadata_urls, token_uri_suffix_str):
   contract = "TDERC721_metadata"
   print("DECLARE " + contract)
-  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto --json")
+  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} --wait-for-acceptance --json")
 
   print("DEPLOY " + contract)
   token_name = str_to_felt(token_name_str)
@@ -95,7 +96,7 @@ def deploy_erc721(token_name_str, token_symbol_str, token_metadata_urls, token_u
   token_uri_suffix = str_to_felt(token_uri_suffix_str)
 
   class_hash = json.loads(out)['class_hash']
-  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto -i {token_name} {token_symbol} {account_addr} 3 {new_token_metadata_urls[0]} {new_token_metadata_urls[1]} {new_token_metadata_urls[2]} {token_uri_suffix} --json")
+  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} -i {token_name} {token_symbol} {account_addr} 3 {new_token_metadata_urls[0]} {new_token_metadata_urls[1]} {new_token_metadata_urls[2]} {token_uri_suffix} --wait-for-acceptance --json")
 
   global tderc721_metadata_addr
   tderc721_metadata_addr = json.loads(out)['contract_address']
@@ -106,11 +107,11 @@ def deploy_erc721(token_name_str, token_symbol_str, token_metadata_urls, token_u
 def deploy_evaluator():
   contract = "Evaluator"
   print("DECLARE " + contract)
-  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto --json")
+  out = run_command(f"protostar -p {network} declare ./build/{contract}.json --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} --wait-for-acceptance --json")
 
   print("DEPLOY " + contract)
   class_hash = json.loads(out)['class_hash']
-  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto -i {tderc20_addr} {player_registry_addr} 3 {tderc721_metadata_addr} {dummytoken_addr} --json")
+  out = run_command(f"protostar -p {network} deploy {class_hash} --salt {salt} --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} -i {tderc20_addr} {player_registry_addr} 3 {tderc721_metadata_addr} {dummytoken_addr} --wait-for-acceptance --json")
 
   global evaluator_addr
   evaluator_addr = json.loads(out)['contract_address']
@@ -122,27 +123,26 @@ def evaluator_set_random_values(list_length, list, column):
   contract = "Evaluator"
   print("Invoke set_random_values on " + contract + " for column " + str(column))
   list_string = ' '.join(str(e) for e in list)
-  run_command(f"protostar -p {network} invoke --contract-address {evaluator_addr} --function set_random_values --account-address {account_addr} --inputs {list_length} {list_string} {column} --private-key-path ./{pkey} --max-fee auto --json")
+  run_command(f"protostar -p {network} invoke --contract-address {evaluator_addr} --function set_random_values --account-address {account_addr} --inputs {list_length} {list_string} {column} --private-key-path ./{pkey} --max-fee {max_fee} --wait-for-acceptance --json")
   return
 
 # Evaluator finish setup
 def evaluator_finish_setup():
   contract = "Evaluator"
   print("Invoke finish setup on " + contract)
-  run_command(f"protostar -p {network} invoke --contract-address {evaluator_addr} --function finish_setup --account-address {account_addr} --private-key-path ./{pkey} --max-fee auto --json")
+  run_command(f"protostar -p {network} invoke --contract-address {evaluator_addr} --function finish_setup --account-address {account_addr} --private-key-path ./{pkey} --max-fee {max_fee} --wait-for-acceptance --json")
   return
 
 # Set Evaluator as admin in ERC20
 def set_evaluator_admin():
   print("set admin and teacher for Evaluator")
-  run_command(f"protostar -p {network} invoke --contract-address {tderc20_addr} --function set_teacher --account-address {account_addr} --inputs {evaluator_addr} 1 --max-fee auto --private-key-path ./{pkey} --json")
-  run_command(f"protostar -p {network} invoke --contract-address {player_registry_addr} --function set_exercise_or_admin --account-address {account_addr} --inputs {evaluator_addr} 1 --max-fee auto --private-key-path ./{pkey} --json")
-
+  run_command(f"protostar -p {network} invoke --contract-address {tderc20_addr} --function set_teacher --account-address {account_addr} --inputs {evaluator_addr} 1 --max-fee {max_fee} --private-key-path ./{pkey} --wait-for-acceptance --json")
+  run_command(f"protostar -p {network} invoke --contract-address {player_registry_addr} --function set_exercise_or_admin --account-address {account_addr} --inputs {evaluator_addr} 1 --max-fee {max_fee} --private-key-path ./{pkey} --wait-for-acceptance --json")
   return
 
 def deploy_all():
-  install()
-  build('./lib/cairo_contracts/src')
+  # install()
+  # build('./lib/cairo_contracts/src')
   # test()
   deploy_TDERC20("ERC721-101", "ERC721-101")
   deploy_players_registry()
